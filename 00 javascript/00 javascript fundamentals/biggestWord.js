ca8d06ceb536d5d3d1cddd6a9f4109e8bc85700a
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2018-10-16 15:06:00 +0200

    Added more exercises

---
+++ 00 javascript/00 javascript fundamentals/biggestWord.js
@@ -0,0 +1,12 @@
+// --- EJERCICIO ------------------------------------------------------------------------
+
+// Crea una función que reciba una frase en formato string y devuelva la palabra más larga.
+// En caso de haber varias con longitud máxima que devuelva siempre la primera.
+
+function biggestWord(phrase) {
+  // Implementation here
+}
+
+// Ejemplo
+console.log(biggestWord("Esta frase puede contener muchas palabras")); // "contener"
+console.log(biggestWord("Ejercicios básicos de JavaScript")); // "JavaScript"
