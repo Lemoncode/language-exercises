996394eaa34de7b42a93ff6acddd7e598d09dccc
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-10-30 11:00:33 +0100

    Slight modifications

--- 00 javascript/00 javascript fundamentals/biggestWord.js
+++ 00 javascript/00 javascript fundamentals/biggestWord.js
@@ -1,7 +1,8 @@
 // --- EJERCICIO ------------------------------------------------------------------------
 
 // Crea una función que reciba una frase en formato string y devuelva la palabra más larga.
-// En caso de haber varias con longitud máxima que devuelva siempre la primera.
+// En caso de haber varias con longitud máxima que devuelva siempre la primera. Ten en cuenta
+// que consideramos una palabra a aquello que esté separado por espacios.
 
 function biggestWord(phrase) {
   // Implementation here
