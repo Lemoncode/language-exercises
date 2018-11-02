20af52e925201b5fb6187b1a1982f89261101061
Javier Calzado <fjcalzadorodriguez@gmail.com>
2018-11-02 11:20:26 +0100

    Reorganization. Added deliverable.md.

--- 00 javascript/01 javascript ES6 and beyond/head.js
+++
@@ -1,7 +0,0 @@
-// --- EJERCICIO ------------------------------------------------------------------------
-
-// Implementa una función head, tal que, dado un array como entrada, extraiga su primer
-// elemento. Utiliza rest / spread.
-// TIP: Se hace en 1 línea.
-
-const head = (/* array*/ ) => {}; // Implementation here.
