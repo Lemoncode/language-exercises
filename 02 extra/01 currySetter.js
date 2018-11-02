9d45c81016d5e414a03adbd8a8f0f5115057418d
Eickhel Mendoza <17008622+Eickhel@users.noreply.github.com>
2018-11-02 22:27:32 +0000

    Update setAge result in comments

--- 02 extra/01 currySetter.js
+++ 02 extra/01 currySetter.js
@@ -27,5 +27,5 @@ const setAge = set(/* ... */);
 const julia = { name: 'Julia', surname: 'Álvarez', age: 19 };
 console.log(setName(julia, 'Ana')); // { name: 'Ana', surname: 'Álvarez', age: 19 };
 console.log(setSurname(julia, 'González')); // { name: 'Julia', surname: 'González', age: 19 };
-console.log(setAge(julia, 25)); // { name: 'Julia', surname: 'Álvarez', age: 19 }
+console.log(setAge(julia, 25)); // { name: 'Julia', surname: 'Álvarez', age: 25 }
 
